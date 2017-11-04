8f234620fbe15c11cbdede97cb489ab759898b2a
Audrey Newman <aen7xa@virginia.edu>
2017-11-04 15:54:14 -0400

    capitalize CharField (fix error)

--- buddysystem/application/forms.py
+++ buddysystem/application/forms.py
@@ -10,7 +10,7 @@ GENDER_CHOICES= [
 
 class SignUpForm(UserCreationForm):
     email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
-    gender = forms.charField(label='Select the appropriate user type:', widget=forms.Select(choices=GENDER_CHOICES))
+    gender = forms.CharField(label='Select the appropriate user type:', widget=forms.Select(choices=GENDER_CHOICES))
     password1 = forms.CharField(label='Password', max_length=254, widget=forms.PasswordInput,
                                 help_text='Your password can\'t be too similar to your other personal information. Your password must contain at least 8 characters. Your password can\'t be a commonly used password. Your password can\'t be entirely numeric.')
 
