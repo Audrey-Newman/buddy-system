9fb6df511c20e81bd5f6ccfd9b142ed4f0a8d557
Audrey Newman <aen7xa@virginia.edu>
2017-11-04 20:13:15 -0400

    extend user model to include gender, departure location, destination; work on ready form

--- buddysystem/application/forms.py
+++ buddysystem/application/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Profile
 
 GENDER_CHOICES= [
     ('Male', 'Male'),
@@ -8,12 +9,27 @@ GENDER_CHOICES= [
     ('Other', 'Other')
     ]
 
+LOC_CHOICES= [
+    ('Clark', 'Clark'),
+    ('Alderman', 'Alderman'),
+    ('Thornton Stacks', 'Thornton Stacks')
+    ]
+
 class SignUpForm(UserCreationForm):
     email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
-    gender = forms.CharField(label='Select the appropriate user type:', widget=forms.Select(choices=GENDER_CHOICES))
+    gender = forms.CharField(label='Gender:', widget=forms.Select(choices=GENDER_CHOICES))
     password1 = forms.CharField(label='Password', max_length=254, widget=forms.PasswordInput,
                                 help_text='Your password can\'t be too similar to your other personal information. Your password must contain at least 8 characters. Your password can\'t be a commonly used password. Your password can\'t be entirely numeric.')
 
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2', 'gender')
+
+class ReadyForm(forms.ModelForm):
+    dep_location = forms.CharField(label='Departure location', widget=forms.Select(choices=LOC_CHOICES))
+    num_companions = forms.IntegerField(label='Desired number of companions')
+    destination = forms.CharField(label='Departure location', widget=forms.Select(choices=LOC_CHOICES))
+
+    class Meta:
+        model = User
+        fields = ('dep_location', 'num_companions', 'destination')
--- buddysystem/application/views.py
+++ buddysystem/application/views.py
@@ -2,12 +2,19 @@ from django.shortcuts import render
 from django.contrib.auth import login, authenticate
 from django.shortcuts import render, redirect
 from .forms import SignUpForm
+from .forms import ReadyForm
+from .models import Profile
+
+# import the logging library
+import logging
+# Get an instance of a logger
+logger = logging.getLogger(__name__)
+#logger.error(message)
 
 def index(request):
     """
     View function for home page of site.
     """
-
     # Render the HTML template index.html with the data in the context variable
     return render(
         request,
@@ -19,12 +26,33 @@ def signup(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save()
+            user.refresh_from_db()  # load the profile instance created by the signal
+            user.profile.gender = form.cleaned_data.get('gender')
+            user.profile.dep_location = 'none'
+            user.profile.destination = 'none'
+            user.save()
             username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
+
             login(request, user)
             return redirect('index')
     else:
         form = SignUpForm()
     return render(request, 'signup.html', {'form': form})
+
+def ready(request):
+    if request.method == 'POST':
+        form = ReadyForm(request.POST)
+        if form.is_valid():
+            u = request.user
+            u.refresh_from_db()
+            #form.save()
+            u.profile.dep_location = form.cleaned_data.get('dep_location')
+            u.profile.destination = form.cleaned_data.get('destination')
+            u.save()
+            return redirect('index')
+    else:
+        form = ReadyForm()
+    return render(request, 'ready.html', {'form': form}) # replace ready.html with index when sarah's done
